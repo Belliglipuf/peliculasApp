d23249554061167691a08cc87d11d04138a0ee8c
sebellid <sebellid@mercadona.es>
2023-01-09 16:59:33 +0100

    Implementando rating

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { Movie } from '../../../../.history/src/app/interfaces/novedades_response.interface_20230104164346';
+import { Movie } from 'src/app/interfaces/novedades_response.interface';
+import { StarRatingComponent } from 'ng-starrating';
+
 
 @Component({
   selector: 'app-card',
@@ -8,7 +10,18 @@ import { Movie } from '../../../../.history/src/app/interfaces/novedades_respons
 })
 export class CardComponent {
 
+
+
+
   @Input()
   movie: Movie;
+
+
+  onRate($event:{oldValue:number, newValue:number, starRating:StarRatingComponent}) {
+    alert(`Old Value:${$event.oldValue},
+      New Value: ${$event.newValue},
+      Checked Color: ${$event.starRating.checkedcolor},
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`);
+  }
 
 }
